17899f9b7a4b9118f101880b89c11cc6bcabbc2c
Bao Xiong <bao.xiong2011@gmail.com>
2021-03-25 19:18:16 -0500

    Added dashboard page and login page.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,33 @@
 const router = require('express').Router();
 const {User}= require('../models')
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+        order: [['name', 'ASC'], ['email', 'ASC']],
+      });
+  
+      const users = userData.map((project) => project.get({ plain: true }));
+  
+      res.render('homepage', {
+        users,
+        //logged in status
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/login', (req, res) => {
+    //if session exists then goes to homepage.
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
