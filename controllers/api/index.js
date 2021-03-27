377c2219604d3fee0c9dd70b2bde4603b77d0ab9
Josie <josiecollinsrc@gmail.com>
2021-03-27 11:12:28 -0500

    Added diaryRoutes to index.js under API

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const diaryRoutes = require('./diaryRoutes')
 
 router.use('/users', userRoutes);
-router.use('/login', loginRoutes);
+router.use('/diary', diaryRoutes);
 
 module.exports = router;
