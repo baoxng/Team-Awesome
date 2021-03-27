8435f7e73c744bcc64e4168290b88594b01f7c9d
Bao Xiong <bao.xiong2011@gmail.com>
2021-03-27 13:19:11 -0500

    Fixed log in rendering page.

--- controllers/urlRoutes.js
+++ controllers/urlRoutes.js
@@ -51,25 +51,25 @@ router.get('/', async (req, res) => {
 //     }
 //   });
 
-// Use withAuth middleware to prevent access to route
-router.get('/login', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
-    });
+// // Use withAuth middleware to prevent access to route
+// router.get('/()', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Post }],
+//     });
 
-    const user = userData.get({ plain: true });
+//     const user = userData.get({ plain: true });
 
-    res.render('homepage', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('homepage', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/signup', (req, res) =>{
     res.render('signup');
