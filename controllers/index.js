12a88ca6447aa141b57d0df781f944ea92e1a236
Bao Xiong <bao.xiong2011@gmail.com>
2021-03-27 11:11:20 -0500

    Fixed error in controllers.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./homeRoutes');
+const urlRoutes = require('./urlRoutes');
 const apiRoutes = require('./api');
 
-router.use('/', homeRoutes);
+router.use('/', urlRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
