491d14446a1bbe151591deeea384bed4cb01b4dc
Josie <josiecollinsrc@gmail.com>
2021-03-29 17:15:22 -0500

    removed unnecessary file

--- seeds/userseed.js
+++
@@ -1,6 +0,0 @@
-const { User } = require('../models');
-const userInfo = [{id: 1, name: "cat", email: "email@gmail.com", password: "1password"}, {id: 2, name: "dog", email: "2email@gmail.com",password: "2password"}]
-
-const seedUsers = () => User.bulkCreate(userInfo, {individualHooks: true, returning: true,});
-
-module.exports = seedUsers;
