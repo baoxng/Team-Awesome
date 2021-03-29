ae148f6834bc6af558e1609b323518a43ce5ac9a
mesahoang <me.sa.hoang@gmail.com>
2021-03-29 05:29:02 -0500

    got the css linked correctly

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.set('view engine', 'handlebars');
 //Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(routes);
 
