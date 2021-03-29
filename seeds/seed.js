ee8a758a2980e16808f1c64636670dcc8572ba77
Josie <josiecollinsrc@gmail.com>
2021-03-29 17:13:42 -0500

    Changed post.js model to match diaryData seed, added ability to seed posts

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Post } = require('../models');
 
 const userInfo = require('./user.json');
+const postInfo = require('./diaryData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +11,8 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  await Post.bulkCreate(postInfo);
 
   process.exit(0);
 };
