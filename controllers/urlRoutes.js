1d1c689c6eec2458e4c8bfb5779cd74350b57b82
Bao Xiong <bao.xiong2011@gmail.com>
2021-03-30 17:24:19 -0500

    Added session into server.

--- controllers/urlRoutes.js
+++ controllers/urlRoutes.js
@@ -43,4 +43,16 @@ router.get('/dashboard', async (req, res) =>{
 });
 
 
+router.post('/login', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.create(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+
 module.exports = router;
