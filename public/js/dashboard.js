4a4844154fd643bf18099e0b300fa61e87f4ded4
mesahoang <me.sa.hoang@gmail.com>
2021-04-01 07:32:19 -0500

    linked js file to handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,23 +1,22 @@
 const postFormHandler = async (event) => {
     event.preventDefault();
   
-    // Collect values from the login form
+    // Collect values from the new diary entry form
     const postName = document.querySelector('#post-email').value.trim();
     const postDate = document.querySelector('#post-date').value.trim();
     const postDescription = document.querySelector('#post-description').value.trim();
-    // const postImage = document.querySelector('#post-image')
   
     if (postName && postDate && postDescription) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/dashboard', {
+      const response = await fetch('/', {
         method: 'POST',
         body: JSON.stringify({ postName, postDate, postDescription }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        console.log(response);
-        // If successful, redirect the browser to the profile page
+        console.log("this worked");
+        // If successful, redirect the browser to the dashboard page
         document.location.replace('/api/users/dashboard');
       } else {
         alert("Failed to create new entry");
--- public/js/signup.js
+++ public/js/signup.js
@@ -15,6 +15,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
+        console.log("this worked");
         document.location.replace('/api/users/dashboard');
       } else {
         alert("User already exists; try again");
