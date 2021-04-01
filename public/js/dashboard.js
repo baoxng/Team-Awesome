fa19e9aec4c11b358defa6efc5c61e1b9d6f6d68
mesahoang <me.sa.hoang@gmail.com>
2021-04-01 06:36:23 -0500

    dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,52 +1,29 @@
-let newPostBtn = document.getElementById("modalBtn");
-
-function InsertRecord() {
-    let postName = document.getElementById('post-name').value;
-    let postDate = document.getElementById('post-date').value;
-    let postDescription = document.getElementById('post-description').value;
-
-//     if (txtid.length != 0 || txtname.length != 0 || txtsalary.length != 0 || txtcity.length != 0) {
-//         var connection = new ActiveXObject("ADODB.Connection");
-//         var connectionstring = "Data Source=.;Initial Catalog=EmpDetail;Persist Security Info=True;User ID=sa;Password=****;Provider=SQLOLEDB";
-//         connection.Open(connectionstring);
-//         var rs = new ActiveXObject("ADODB.Recordset");
-//         rs.Open("insert into Emp_Info values('" + txtid + "','" + txtname + "','" + txtsalary + "','" + txtcity + "')", connection);
-//         alert("Insert Record Successfuly");
-//         txtid.value = " ";
-//         connection.close();
-//     }
-//     else {
-//         alert("Please Enter Employee \n Id \n Name \n Salary \n City ");
-//     }
-// }
-
-// document.querySelector(newEntryBtn).addEventListener("click", () => {
-//         //adding element data to local storage
-//         let newPost = {
-//           postName: JSON.stringify(name),
-//           postDate: JSON.stringify(date),
-//           postDescription: JSON.stringify(description)
-//         }
-//         savePlaces.push(newPost);
-//         localStorage.setItem("results-saved-1", JSON.stringify(savePosts));
-//         // run function to load results
-//         loadResults();    
-// });
-
-// function loadResults() {
-//     var savePosts = JSON.parse(localStorage.getItem("results-saved-1"));
-//     $(".saveItem").empty();
-//     for (var i = 0; i < savePosts.length; i++) {
-//       if (savePosts[i].postName) {
-//         let postNameEl = savePosts[i].postName;
-//         let postDateEl = savePosts[i].postDate;
-//         let postDescriptionEl = savePosts[i].postDescription;
-
-//         //append to the page
-//         let p = document.getElementById("modalBtn");
-//         let postList = $("<li class='name-title'>");
-//         postList.append(postNameEl);
-//         p.append(postList);
-
-//   }};
-// }
+const postFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const postName = document.querySelector('#post-email').value.trim();
+    const postDate = document.querySelector('#post-date').value.trim();
+    const postDescription = document.querySelector('#post-description').value.trim();
+  
+    if (postName && postDate && postDescription) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ postName, postDate, postDescription }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        console.log(response);
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/api/users/dashboard');
+      } else {
+        alert("Failed to create new entry");
+      }
+    }
+  };
+  
+  document
+    .querySelector('#post-form')
+    .addEventListener('submit', postFormHandler);
