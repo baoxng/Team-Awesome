5249d536955ecc354f953d0de1999df31b76f22e
mesahoang <me.sa.hoang@gmail.com>
2021-04-01 07:07:30 -0500

    added script to dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,10 +5,11 @@ const postFormHandler = async (event) => {
     const postName = document.querySelector('#post-email').value.trim();
     const postDate = document.querySelector('#post-date').value.trim();
     const postDescription = document.querySelector('#post-description').value.trim();
+    // const postImage = document.querySelector('#post-image')
   
     if (postName && postDate && postDescription) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/users/dashboard', {
         method: 'POST',
         body: JSON.stringify({ postName, postDate, postDescription }),
         headers: { 'Content-Type': 'application/json' },
